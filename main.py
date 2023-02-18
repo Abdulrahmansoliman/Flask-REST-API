b89e434005d592d1751ce113484a9468b19effc5
Abdulrahman Soliman <abdulrahmansoliman391@gmail.com>
2023-02-19 00:03:29 +0200

    Handling deleting requests

--- main.py
+++ main.py
@@ -6,28 +6,9 @@ api = Api(app)
 
 
 video_put_args = reqparse.RequestParser()
-"""
-We use reqparse in Flask to parse and validate request data(such as form data, query parameters, and JSON payloads) in a clean and 
-consistent way. reqparse is a module provided by the Flask-RESTful extension that allows you to define a set of request arguments 
-and apply validation rules to each of them. This helps to ensure that your API only accepts valid and well-formed data from clients.
-
-Here are some reasons why we use reqparse in Flask:
-It provides a convenient and consistent way to define request arguments and validation rules.
-It automatically handles data type conversion and formatting, so you don't have to write repetitive code for each request argument.
-It can handle complex data structures such as nested JSON objects and arrays.
-It raises informative errors when validation fails, making it easier to debug issues with incoming requests.
-It is well-documented and widely used in the Flask community, so it's easy to find help and examples online
-"""
-
 video_put_args.add_argument("name", type=str, help="Name of the video is required", required=True)
 video_put_args.add_argument("views", type=int, help="Views of the video")
 video_put_args.add_argument("likes", type=int, help="Likes on the video")
-
-"""
-we use the parse_args() method of the RequestParser object to parse the request arguments and validate them according to the rules
-we defined earlier. If any validation errors occur (such as a missing required argument or an argument with an invalid data type), 
-parse_args() will raise a BadRequest exception.
-"""
 
 videos = {"tim":{"age":19 , "gender":"male"},
          "John":{"age":21, "gender":"female"},
@@ -36,18 +17,26 @@ videos = {"tim":{"age":19 , "gender":"male"},
 def abort_if_video_id_doesnt_exist(video_id):  
     if video_id not in videos:
         abort(404, message="Could not find video...")
-        
+
+def abort_if_video_exists(video_id):
+    if video_id in videos:
+        abort(409, message="Video already exists with that ID...")        
 class Video(Resource):
     def get(self, video_id):
         abort_if_video_id_doesnt_exist(video_id)
         return videos[video_id]
 
     def put(self, video_id):
+        abort_if_video_exists(video_id)
         args = video_put_args.parse_args()
         videos[video_id]=args    
         return(args, 201)
     
-    
+    def delete (self, video_id):
+        abort_if_video_id_doesnt_exist(video_id)
+        del videos[video_id]
+        return '', 204
+
 api.add_resource(Video, "/video/<int:video_id>")
 
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -2,10 +2,20 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.put(BASE + "video/1" ,{'name':'Scent', 'views':100, 'likes':10})
-print(response.json())
+
+data = [{'likes': 10, 'name': 'Nothing good', 'views': 10000},
+        {'likes': 10, 'name': 'bad days', 'views': 800},
+        {'likes': 10, 'name': 'HELP!', 'views': 2000},
+]
+
+for i in range(len(data)):
+    response = requests.put(BASE + "video/" + str(i), data[i])
+    print(response.json())
 
 input()
+response = requests.delete(BASE + "video/0")
+print (response.content)
 
+input()
 response = requests.get(BASE + "video/1")
 print(response.json())
